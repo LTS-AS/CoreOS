c571a43b17a8e23e220cb1713e2c8e89bb65ecc9
Havard <havard.line@gmail.com>
2015-10-11 22:22:42 +0200

    Bugfix

--- deploy_openerp.sh
+++ deploy_openerp.sh
@@ -1,5 +1,5 @@
 # This scrit rebuild and deploys havardline/debian-openerp-no
-docker -t havardline/debian-openerp-no ./fleet/openerp
+docker build -t havardline/debian-openerp-no ./fleet/openerp
 docker push havardline/debian-openerp-no
 
 # Maybe also do something like this in the future?
