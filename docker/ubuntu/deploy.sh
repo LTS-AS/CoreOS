dbbfa62a58f5d48ced8c34880f01562c790a7fc5
Havard <havard.line@gmail.com>
2016-05-23 21:16:30 +0200

    Added git pull, added comments

--- docker/ubuntu/deploy.sh
+++ docker/ubuntu/deploy.sh
@@ -1,4 +1,8 @@
+# Stop and remove old container
 docker stop ubuntu_unsecure && \
   docker rm ubuntu_unsecure
-docker build -t havardline/ubuntu_unsecure . && \
+  
+# Pull latest code, build and run container
+git pull && \
+  docker build -t havardline/ubuntu_unsecure . && \
   docker run -d -P  --name ubuntu_unsecure havardline/ubuntu_unsecure 
