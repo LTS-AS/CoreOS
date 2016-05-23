b059fe1aacdca361be71e1efc48a158b2dff6241
Havard <havard.line@gmail.com>
2016-05-23 21:03:51 +0200

    removing container before new build

--- docker/ubuntu/deploy.sh
+++ docker/ubuntu/deploy.sh
@@ -1,2 +1,4 @@
+docker stop ubuntu_unsecure && \
+  docker rm ubuntu_unsecure
 docker build -t havardline/ubuntu_unsecure . && \
   docker run -d -P  --name ubuntu_unsecure havardline/ubuntu_unsecure 
